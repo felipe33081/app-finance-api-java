b7041d4129d3a30a04f2a3e6e364ef393f5fc2d2
Felipe Pereira <Felipe33081@hotmail.com>
2024-11-04 23:29:47 -0300

    adjustments DB

--- src/test/java/com/dev/estacio/finance/AppFinanceTests.java
+++ src/test/java/com/dev/estacio/finance/AppFinanceTests.java
@@ -2,8 +2,10 @@ package com.dev.estacio.finance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AppFinanceTests {
 
     @Test
